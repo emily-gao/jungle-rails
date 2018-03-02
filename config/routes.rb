78849ea09b053fcc7a03581179209cfc079f9303
Emily Gao <emily.s.gao@gmail.com>
2018-03-01 19:33:37 -0700

    rename register/login/logout routes from default

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'register', to: 'users#new', as: 'register'
+  get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+
   root to: 'products#index'
 
   namespace :admin do
