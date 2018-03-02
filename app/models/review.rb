8abf56faf69ecd237e0e4cc412427cc9bdffcf9d
Emily <emily.s.gao@gmail.com>
2018-03-02 13:47:54 -0700

    update: description in a review is not required

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,6 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
 
-  validates :description, presence: true
   validates :rating, presence: true, numericality: {greater_than: 0, less_than: 6}
 
 end
