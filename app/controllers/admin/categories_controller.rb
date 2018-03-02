e3e895b35bfff05820a1fb9ef00a053276567012
Emily <emily.s.gao@gmail.com>
2018-03-02 12:35:36 -0700

    add basic authentication to all 3 nested resources of namespace admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CategoriesController < ApplicationController
 
+  http_basic_authenticate_with name: Rails.configuration.admin[:username], password: Rails.configuration.admin[:password]
+  
   def index
     @categories = Category.all
   end
