ab8f9bac6c40f1b5d777f18a5063e93d89bf3fa1
Emily <emily.s.gao@gmail.com>
2018-03-02 09:45:48 -0700

    add action to send receipt_email when an order is created

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
 
+  before_filter :authorize
+
   def show
     @order = Order.find(params[:id])
     @line_items = LineItem.where(order_id: params[:id])
@@ -10,6 +12,7 @@ class OrdersController < ApplicationController
     order  = create_order(charge)
 
     if order.valid?
+      OrderMailer.receipt_email(order).deliver_later
       empty_cart!
       redirect_to order, notice: 'Your Order has been placed.'
     else
